2af5cfe4161e012adef8a66956d26b5d5b4c471b
amgestiehr3 <web@ymdigitallabc5.3_1>
2015-06-24 13:17:46 -0400

    scope todos

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
 var app = angular.module('Todo', []);
 app.controller('TodoCtrl', function($scope) {
-	$scope.message = 'Angular is pretty cool.';
+	$scope.message = 'Hi everyone i am happy.';
+	
+	$scope.todos = [
+    'Learn Sketch', 
+    'Look at Dribbble and feel inferior',
+    'Actually learn how to use the Pen tool'
+ 	 ];
 });  
