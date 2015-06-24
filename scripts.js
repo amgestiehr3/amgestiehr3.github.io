f97accbaeb23471c3d430524d8900fb2e07cc8c5
amgestiehr3 <web@ymdigitallabc5.3_1>
2015-06-24 14:11:53 -0400

    zac's change

--- scripts.js
+++ scripts.js
@@ -7,10 +7,11 @@ app.controller('TodoCtrl', function($scope) {
     'Look at Dribbble and feel inferior',
     'Actually learn how to use the Pen tool'
  	 ];
+ 	 
  	$scope.done = function(todo) {
     var indexOf = $scope.todos.indexOf(todo);
     if (indexOf !== -1) {
       $scope.todos.splice(indexOf, 1);
     }
-  };  
+    };  
 });  
