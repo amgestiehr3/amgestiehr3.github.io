b4f2378505fc87a3f457aa6b95cb06ebf6cf88bf
amgestiehr3 <web@ymdigitallabc5.3_1>
2015-06-25 13:59:44 -0400

    change

--- scripts.js
+++ scripts.js
@@ -14,12 +14,17 @@ var app = angular.module('Todo', []);
 app.controller('TodoCtrl', function($scope) {
 	$scope.message = 'Hi everyone i am happy.';
 	
+	var todosStorage=localstorage.ge;Items("todos");
+	
+	if(todosStorage!=null){
+	$scope.todos=todosStorage;
+	} else { 
 	$scope.todos = [
-    'Learn Sketch', 
-    'Look at Dribbble and feel inferior',
-    'Actually learn how to use the Pen tool'
- 	 ];
- 	 
+    	'Learn Sketch', 
+    	'Look at Dribbble and feel inferior',
+    	'Actually learn how to use the Pen tool'
+ 	 	];
+ 	 }
  	$scope.done = function(todo) {
     var indexOf = $scope.todos.indexOf(todo);
     if (indexOf !== -1) {
